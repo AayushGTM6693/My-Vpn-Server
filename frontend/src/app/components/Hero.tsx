675e78c6f1ad69433078259a7410d6ef4c435f43
Aayush Gauutam <professional.atus6693@gmail.com>
2025-06-25 16:47:30 +0545

    ui fix

--- frontend/src/app/components/Hero.tsx
+++ frontend/src/app/components/Hero.tsx
@@ -3,7 +3,9 @@
 export default function Hero() {
   return (
     <header className="text-center space-y-4">
-      <h1 className="text-5xl font-bold text-blue-400">Welcome to MyVPN 🌐</h1>
+      <h1 className="text-5xl font-bold text-blue-400">
+        Welcome to My Singapore VPN🌐
+      </h1>
       <p className="text-lg text-gray-300">
         Secure. Fast. Free. Access your internet like never before.
       </p>
